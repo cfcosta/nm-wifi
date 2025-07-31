cddb566d58513326abd8f84d3f49ed6773bb3b5a
Cainã Costa <me@cfcosta.com>
2025-07-30 23:35:39 -0300

    hello: world

---
+++ flake.nix
@@ -0,0 +1,114 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+      rust-overlay,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        inherit (pkgs) mkShell;
+
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+
+        formatter =
+          (treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "flake.nix";
+
+            settings = {
+              allow-missing-formatter = true;
+              verbose = 0;
+
+              global.excludes = [ "*.lock" ];
+
+              formatter = {
+                nixfmt.options = [ "--strict" ];
+                rustfmt.package = rust;
+              };
+            };
+
+            programs = {
+              nixfmt.enable = true;
+              prettier.enable = true;
+              rustfmt = {
+                enable = true;
+                package = rust;
+              };
+              taplo.enable = true;
+            };
+          }).config.build.wrapper;
+
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+
+          hooks = {
+            deadnix.enable = true;
+            nixfmt-rfc-style.enable = true;
+            treefmt = {
+              enable = true;
+              package = formatter;
+            };
+          };
+        };
+
+        packages.default = rustPlatform.buildRustPackage {
+          name = "nm-wifi";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      in
+      {
+        inherit packages;
+
+        formatter = formatter;
+
+        checks = { inherit pre-commit-check; };
+
+        devShells.default = mkShell {
+          name = "nm";
+
+          buildInputs = with pkgs; [
+            rust
+            formatter
+
+            cargo-nextest
+            cargo-mutants
+            bacon
+
+            pkg-config
+            dbus.dev
+          ];
+        };
+      }
+    );
+}
